92e75fa88ff9c6f136210ba1779320ece63ead1b
BigdawgRav <raveenseshan@live.com>
2021-08-27 23:55:37 +1000

    fixed

--- FormGenxFrontEnd/src/constants/Urls.js
+++ FormGenxFrontEnd/src/constants/Urls.js
@@ -1,10 +1,10 @@
 
 //Production
-// export const backEndBaseUrl = "https://formgenx.herokuapp.com";
-
+// export const backEndBaseUrl = "https://formgenx.herokuapp.com"
 
 //Staging
 export const backEndBaseUrl = "https://formgenxstaging.herokuapp.com";
 
 // local
 // export const backEndBaseUrl = "http://localhost:8000";
+
